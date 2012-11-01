22544eb3e168d57c4a36e73c5703c818c566720a
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-01 11:58:37 +0000

    Post-update to push changes to live site

---
+++ _post-update.php
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo
+echo "**** Pulling changes into Live [Github's post-update hook]"
+echo
+
+cd $HOME/www || exit
+unset GIT_DIR
+git pull origin master
+
+exec git-update-server-info
