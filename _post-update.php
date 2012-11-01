bbf19ec04dae5e669e8011e851f1eb5a281fff07
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-01 16:19:07 +0000

    Update to post-update

--- _post-update.php
+++ _post-update.php
@@ -1,15 +1,5 @@
 <?php
 
-#!/bin/sh
-
-echo
-echo "**** Pulling changes into Live [Github's post-update hook]"
-echo
-
-cd $HOME/www || exit
-unset GIT_DIR
-git pull origin master
-
-exec git-update-server-info
+'git pull origin master'
 
 ?>
